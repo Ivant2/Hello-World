e2c9b9f12a62da58e29b8922b3c7b9223e49ad1f
Ivant2 <32110248+Ivant2@users.noreply.github.com>
2017-10-05 13:45:53 -0600

    lnuyfu77uy6

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string hello = Console.ReadLine();
+            Console.WriteLine("Jayden has Herpes");
+        }
+    }
+}
