9a7ab06aaa2d07b7f6fac849f92b66a448b1cd88
Ivant2 <32110248+Ivant2@users.noreply.github.com>
2017-10-10 13:06:28 -0600

    rte

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,7 +7,7 @@ namespace HelloWorld
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-           
+            Console.ReadLine();
         }
     }
 }
